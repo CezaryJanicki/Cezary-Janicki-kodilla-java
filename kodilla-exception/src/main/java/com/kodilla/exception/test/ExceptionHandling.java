c1e5072744b0e34b9ef158fec3e8f5135b45ed2b
CezaryJanicki <cezary.janicki@gmail.com>
2020-08-04 07:47:23 +0200

    Task-8-3

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,14 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void main(String args[]) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try {
+            secondChallenge.probablyIWillThrowException(3.0, 3.0);
+        } catch (Exception e) {
+            System.out.println("I have found the exception " + e + " and handled it!");
+        } finally {
+            System.out.println("What was the exception?");
+        }
+    }
+}
