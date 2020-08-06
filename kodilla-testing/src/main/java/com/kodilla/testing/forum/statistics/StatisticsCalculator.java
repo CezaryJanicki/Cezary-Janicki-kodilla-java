b0c668dbef2ed33d1e7221ec47e9a38b5a2014aa
CezaryJanicki <cezary.janicki@gmail.com>
2020-08-06 22:58:01 +0200

    updating kodilla examples

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsCalculator.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.forum.statistics;
+
+import com.kodilla.testing.forum.statistics.Statistics;
+
+public class StatisticsCalculator {
+
+    public void calculateAdvStatistics(Statistics statistics) {};
+
+    public void showStatistics() {};
+
+}
