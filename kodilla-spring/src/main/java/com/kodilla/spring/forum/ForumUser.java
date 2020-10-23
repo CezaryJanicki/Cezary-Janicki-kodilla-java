2ffd081baa04c584f12cbe98dc58d93de202a7cf
CezaryJanicki <cezary.janicki@gmail.com>
2020-10-23 19:37:45 +0200

    after code review

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ForumUser {
-    String username = "John Smith";
+    String username;
+
+    public ForumUser() {
+        this.username = "John Smith";
+    }
 
     public String getUsername() {
         return username;
