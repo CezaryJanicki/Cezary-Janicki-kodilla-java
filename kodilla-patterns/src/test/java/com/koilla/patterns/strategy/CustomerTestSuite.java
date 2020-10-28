6f510da2252551ee5d29549cbeef069a22333bf7
CezaryJanicki <cezary.janicki@gmail.com>
2020-10-28 20:38:13 +0100

    Strategy design patterns Kodilla example

---
+++ kodilla-patterns/src/test/java/com/koilla/patterns/strategy/CustomerTestSuite.java
@@ -0,0 +1,50 @@
+package com.koilla.patterns.strategy;
+
+import com.kodilla.patterns.strategy.CorporateCustomer;
+import com.kodilla.patterns.strategy.Customer;
+import com.kodilla.patterns.strategy.IndividualCustomer;
+import com.kodilla.patterns.strategy.IndividualYoungCustomer;
+import com.kodilla.patterns.strategy.predictors.AggresivePredictor;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.SQLOutput;
+
+public class CustomerTestSuite {
+
+    @Test
+    public void testDefaultInvestingStrategies() {
+        //Given
+        Customer steven = new IndividualCustomer("Steven Links");
+        Customer john = new IndividualYoungCustomer("John Hemerald");
+        Customer kodilla = new CorporateCustomer("Kodilla");
+        //When
+        String stevenShouldBuy = steven.predict();
+        System.out.println("Steve should: " + stevenShouldBuy);
+        String johnShouldBuy = john.predict();
+        System.out.println("John should: " + johnShouldBuy);
+        String kodillaShouldBuy = kodilla.predict();
+        System.out.println("Kodilla should: " + kodillaShouldBuy);
+
+        //Then
+        Assert.assertEquals("[Conservative predictor] Buy debentures of XYZ", stevenShouldBuy);
+        Assert.assertEquals("[Aggresive predictor] Buy stock of XYZ", johnShouldBuy);
+        Assert.assertEquals("[Balanced predictor] Buy shared units of Fund XYZ", kodillaShouldBuy);
+    }
+
+    @Test
+    public void testIndividualInvestingStrategy() {
+        //Given
+        Customer steven = new IndividualCustomer("Steven Links");
+
+        //When
+        String stevenShouldBuy = steven.predict();
+        System.out.println("Steven should: " + stevenShouldBuy);
+        steven.setBuyingStrategy(new AggresivePredictor());
+        stevenShouldBuy = steven.predict();
+        System.out.println("Steven now should: " + stevenShouldBuy);
+
+        //Then
+        Assert.assertEquals("[Aggresive predictor] Buy stock of XYZ", stevenShouldBuy);
+    }
+}
